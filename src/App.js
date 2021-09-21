8f2d17c1d979569f95ee9e17f9f0efe22f7a0e61
Sanjith Kumar <53137451+sanjith-kumar-048@users.noreply.github.com>
2021-09-21 10:19:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-//import "./App.css";
+import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Layout from "./components/layouts/Layout";
 import HomePage from "./pages/HomePage";
