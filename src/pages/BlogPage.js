ccf5f1c2025a7ae02f7fa7a8601f1fcc8717b91e
Vigneshwara-T <91001688+Vigneshwara-T@users.noreply.github.com>
2021-10-19 16:27:01 +0530

    Changes in css for Blogs and Uncat Pages

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -56,11 +56,11 @@ export default function BlogPage() {
     <div>
       
       <div className={classes.slip}>
-        <img className={classes.slip_img} src="https://hennacrafts.com/wp-content/uploads/2021/03/9.jpg" />
+        <img className={classes.slip__img} src="https://hennacrafts.com/wp-content/uploads/2021/03/9.jpg" />
         <div className={classes.slip_body}>
-          <p className={classes.slip_title}>
+          <h1>
             Foot Mehendi For Your Inspiration
-          </p>
+          </h1>
           <p className={classes.slip_description}>
             The Best Of The Best Bridal Foot Mehendi For Your Inspiration My
             last post got me thinking, we all have seen amazing bridal mehendi
@@ -75,9 +75,9 @@ export default function BlogPage() {
       </div>
 
       <div className={classes.slip}>
-        <img className={classes.slip_img} src="https://hennacrafts.com/wp-content/uploads/2021/03/word-image-41-1024x576.jpeg" />
+        <img className={classes.slip__img} src="https://hennacrafts.com/wp-content/uploads/2021/03/word-image-41-1024x576.jpeg" />
         <div className={classes.slip_body}>
-          <p className={classes.slip_title}>WHICH HENNA IS GOOD FOR HAIR</p>
+          <h1>WHICH HENNA IS GOOD FOR HAIR</h1>
           <p className={classes.slip_description}>
             WHICH HENNA IS GOOD FOR HAIR? Henna is a boon to those with product
             sensitivities, allergies, and other medical conditions. In fact,
@@ -93,12 +93,12 @@ export default function BlogPage() {
       </div>
 
       <div className={classes.slip}>
-        <img className={classes.slip_img} src="https://hennacrafts.com/wp-content/uploads/2021/03/word-image-38-1024x683.jpeg" />
+        <img className={classes.slip__img} src="https://hennacrafts.com/wp-content/uploads/2021/03/word-image-38-1024x683.jpeg" />
         <div className={classes.slip_body}>
-          <p className={classes.slip_title}>
+          <h1>
             SHOULD I GO DARKER EVERYTHING YOU NEED TO KNOW ABOUT HENNA FOR
             BRUNETTES
-          </p>
+          </h1>
           <p className={classes.slip_description}>
             SHOULD I GO DARKER? EVERYTHING YOU NEED TO KNOW ABOUT HENNA FOR
             BRUNETTES Switching up your hair color can be exciting, especially
@@ -113,11 +113,11 @@ export default function BlogPage() {
       </div>
 
       <div className={classes.slip}>
-        <img className={classes.slip_img} src="https://hennacrafts.com/wp-content/uploads/2021/03/low-maintenance-hair-color-trends-or-teen-vogue-1024x1024.jpeg" />
+        <img className={classes.slip__img} src="https://hennacrafts.com/wp-content/uploads/2021/03/low-maintenance-hair-color-trends-or-teen-vogue-1024x1024.jpeg" />
         <div className={classes.slip_body}>
-          <p className={classes.slip_title}>
+          <h1>
             NATURAL HAIR COLOR 101 HENNA HAIR DYE TIPS
-          </p>
+          </h1>
           <p className={classes.slip_description}>
             NATURAL HAIR COLOR 101 HENNA HAIR DYE TIPS" description="NATURAL
             HAIR COLOR 101: HENNA HAIR DYE TIPS Natural Hair Color 101: Henna
@@ -134,11 +134,11 @@ export default function BlogPage() {
       </div>
 
       <div className={classes.slip}>
-        <img className={classes.slip_img} src="https://hennacrafts.com/wp-content/uploads/2021/03/word-image-36-1024x725.jpeg" />
+        <img className={classes.slip__img} src="https://hennacrafts.com/wp-content/uploads/2021/03/word-image-36-1024x725.jpeg" />
         <div className={classes.slip_body}>
-          <p className={classes.slip_title}>
+          <h1>
             How to Use Tea Tree Oil for Hair Growth
-          </p>
+          </h1>
           <p className={classes.slip_description}>
             How to Use Tea Tree Oil for Hair Growth? By Beatuy Epic on September
             21, 2019 on Hair, Hair Growth Tea tree oil has been used from
@@ -154,11 +154,11 @@ export default function BlogPage() {
       </div>
 
       <div className={classes.slip}>
-        <img className={classes.slip_img} src="https://hennacrafts.com/wp-content/uploads/2021/03/word-image-32-1024x768.jpeg" />
+        <img className={classes.slip__img} src="https://hennacrafts.com/wp-content/uploads/2021/03/word-image-32-1024x768.jpeg" />
         <div className={classes.slip_body}>
-          <p className={classes.slip_title}>
+          <h1>
             HOW TO GET THE BEST RESULTS FROM DARK BROWN HENNA HAIR COLOR TIPS
-          </p>
+          </h1>
           <p className={classes.slip_description}>
             HOW TO GET THE BEST RESULTS FROM DARK BROWN HENNA HAIR COLOR Whether
             you’re a longtime user or new to henna for hair, you’ve probably
@@ -173,11 +173,11 @@ export default function BlogPage() {
       </div>
 
       <div className={classes.slip}>
-        <img className={classes.slip_img} src="https://hennacrafts.com/wp-content/uploads/2021/03/sally-essential-colour-skills-refresher-course-or-s.jpeg" />
+        <img className={classes.slip__img} src="https://hennacrafts.com/wp-content/uploads/2021/03/sally-essential-colour-skills-refresher-course-or-s.jpeg" />
         <div className={classes.slip_body}>
-          <p className={classes.slip_title}>
+          <h1>
             GET THE 6 BEST HAIR COLORS FOR WINTER 2020 FROM HENNA
-          </p>
+          </h1>
           <p className={classes.slip_description}>
             GET THE 6 BEST HAIR COLORS FOR WINTER 2020 FROM HENNA HOT HENNA
             COLORS FOR THOSE COLD WINTER MONTHS 2020 has been quite a year —
@@ -192,11 +192,11 @@ export default function BlogPage() {
       </div>
 
       <div className={classes.slip}>
-        <img className={classes.slip_img} src="https://hennacrafts.com/wp-content/uploads/2021/03/51-beautiful-designs-for-hands-or-mahndi-desgins.jpeg" />
+        <img className={classes.slip__img} src="https://hennacrafts.com/wp-content/uploads/2021/03/51-beautiful-designs-for-hands-or-mahndi-desgins.jpeg" />
         <div className={classes.slip_body}>
-          <p className={classes.slip_title}>
+          <h1>
             51 Beautiful Mylanchi designs for hands Kerala mehndi designs
-          </p>
+          </h1>
           <p className={classes.slip_description}>
             51 Beautiful Mylanchi designs for hands || Kerala mehndi designs It
             doesn’t matter if you love henna or hate it, it is one thing you
@@ -211,11 +211,11 @@ export default function BlogPage() {
       </div>
 
       <div className={classes.slip}>
-        <img className={classes.slip_img} src="https://hennacrafts.com/wp-content/uploads/2021/03/word-image-24.jpeg" />
+        <img className={classes.slip__img} src="https://hennacrafts.com/wp-content/uploads/2021/03/word-image-24.jpeg" />
         <div className={classes.slip_body}>
-          <p className={classes.slip_title}>
+          <h1>
             Eucalyptus Oil A Natural Wake-me-up
-          </p>
+          </h1>
           <p className={classes.slip_description}>
             Eucalyptus Oil: A Natural Wake-me-up For You and Your Skin [History,
             Uses and Skin Benefits] We may be familiar with eucalyptus oil for
@@ -230,11 +230,11 @@ export default function BlogPage() {
       </div>
 
       <div className={classes.slip}>
-        <img className={classes.slip_img} src="https://hennacrafts.com/wp-content/uploads/2021/03/9-1.jpg" />
+        <img className={classes.slip__img} src="https://hennacrafts.com/wp-content/uploads/2021/03/9-1.jpg" />
         <div className={classes.slip_body}>
-          <p className={classes.slip_title}>
+          <h1>
             Benefits of Henna For Hair Health
-          </p>
+          </h1>
           <p className={classes.slip_description}>
             Eucalyptus Oil: A Natural Wake-me-up For You and Your Skin [History,
             Uses and Skin Benefits] We may be familiar with eucalyptus oil for
@@ -249,11 +249,11 @@ export default function BlogPage() {
       </div>
 
       <div className={classes.slip}>
-        <img className={classes.slip_img} src="https://hennacrafts.com/wp-content/uploads/2021/03/image-result-for-eucalyptus.jpeg" />
+        <img className={classes.slip__img} src="https://hennacrafts.com/wp-content/uploads/2021/03/image-result-for-eucalyptus.jpeg" />
         <div className={classes.slip_body}>
-          <p className={classes.slip_title}>
+          <h1>
             Eucalyptus Oil for Cold and Flu Season
-          </p>
+          </h1>
           <p className={classes.slip_description}>
             Eucalyptus Oil for Cold and Flu Season Eucalyptus essential oil is a
             wonderful multi-faceted oil with many therapeutic uses. You can find
@@ -268,11 +268,11 @@ export default function BlogPage() {
       </div>
 
       <div className={classes.slip}>
-        <img className={classes.slip_img} src="https://hennacrafts.com/wp-content/uploads/2021/03/word-image-2.jpeg" />
+        <img className={classes.slip__img} src="https://hennacrafts.com/wp-content/uploads/2021/03/word-image-2.jpeg" />
         <div className={classes.slip_body}>
-          <p className={classes.slip_title}>
+          <h1>
             17 Spine Tattoo Designs That Will Chill You To The Bone
-          </p>
+          </h1>
           <p className={classes.slip_description}>
             17 Spine Tattoo Designs That Will Chill You To The Bone"
             description="17 Spine Tattoo Designs That Will Chill You To The Bone
@@ -288,11 +288,11 @@ export default function BlogPage() {
       </div>
 
       <div className={classes.slip}>
-        <img className={classes.slip_img} src="https://hennacrafts.com/wp-content/uploads/2021/03/oral-health-systems-in-portugal-sweden-and-roman.png" />
+        <img className={classes.slip__img} src="https://hennacrafts.com/wp-content/uploads/2021/03/oral-health-systems-in-portugal-sweden-and-roman.png" />
         <div className={classes.slip_body}>
-          <p className={classes.slip_title}>
+          <h1>
             13 USES FOR TEA TREE OIL THAT WILL CHANGE YOUR LIFE
-          </p>
+          </h1>
           <p className={classes.slip_description}>
             17 Spine Tattoo Designs That Will Chill You To The Bone"
             description="13 USES FOR TEA TREE OIL THAT WILL CHANGE YOUR LIFE Not
@@ -309,11 +309,11 @@ export default function BlogPage() {
       </div>
 
       <div className={classes.slip}>
-        <img className={classes.slip_img} src="https://hennacrafts.com/wp-content/uploads/2021/03/word-image-6-1024x683.jpeg" />
+        <img className={classes.slip__img} src="https://hennacrafts.com/wp-content/uploads/2021/03/word-image-6-1024x683.jpeg" />
         <div className={classes.slip_body}>
-          <p className={classes.slip_title}>
+          <h1>
             20 Uses for Lavender Essential Oil
-          </p>
+          </h1>
           <p className={classes.slip_description}>
             Whether you are looking for a way to soothe a bug bite, calm an
             earache, make some homemade body products, or just unwind at the end
@@ -328,11 +328,11 @@ export default function BlogPage() {
       </div>
 
       <div className={classes.slip}>
-        <img className={classes.slip_img} src="https://hennacrafts.com/wp-content/uploads/2021/03/indigo-plants-wholesale-price-and-mandi-rate-for-i-1024x576.jpeg" />
+        <img className={classes.slip__img} src="https://hennacrafts.com/wp-content/uploads/2021/03/indigo-plants-wholesale-price-and-mandi-rate-for-i-1024x576.jpeg" />
         <div className={classes.slip_body}>
-          <p className={classes.slip_title}>
+          <h1>
             8 Brilliant Benefits Of Using Indigo Powder For Hair
-          </p>
+          </h1>
           <p className={classes.slip_description}>
             W8 Brilliant Benefits Of Using Indigo Powder For Hair WHAT IS INDIGO
             POWDER? Indigo powder is a dye that comes from the crushed leaves of
@@ -347,11 +347,11 @@ export default function BlogPage() {
       </div>
 
       <div className={classes.slip}>
-        <img className={classes.slip_img} src="https://hennacrafts.com/wp-content/uploads/2021/03/how-to-use-henna-1024x683.jpeg" />
+        <img className={classes.slip__img} src="https://hennacrafts.com/wp-content/uploads/2021/03/how-to-use-henna-1024x683.jpeg" />
         <div className={classes.slip_body}>
-          <p className={classes.slip_title}>
+          <h1>
             5 Basic Steps To Apply Henna For Grey Hair
-          </p>
+          </h1>
           <p className={classes.slip_description}>
             If you are looking for hair that is long, strong, smooth and thick,
             then artificial products will never suit you. It will yield better
